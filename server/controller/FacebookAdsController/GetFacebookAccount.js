2f8bde3f537e58e14b40a943c8ddc043e7bdaa94
Akash kumar <akashbhopewal@gmail.com>
2022-03-23 11:22:48 +0530

    Get all needed data form Facebook ads account

---
+++ server/controller/FacebookAdsController/GetFacebookAccount.js
@@ -0,0 +1,37 @@
+
+const subscription = require("../../model/Subscription");
+const FacebookAds = require("../../model/FacebookAds");
+const GetFacebookData = async(req,res)=>{
+    try {
+        const {access_token,account_id} = req.body;
+        console.log(access_token,account_id)
+
+        if(!access_token || !account_id){
+            return res.status(200).json({message:"access_token and account_id must be set in req body"});
+        }
+
+        console.log("hello")
+        const subscrip = new subscription({
+            userId: req.user.id,
+            lastProcess: new Date().setDate(new Date().getDate()),
+            nextPayment: new Date().setDate(new Date().getDate() + 7),
+            status: true
+          });
+
+          const subs = await subscrip.save();
+
+          const data =  new FacebookAds({
+            subsId: subs._id,
+            access_Token:access_token,
+            account_id:account_id
+          });
+
+          const result = await data.save();
+          return res.status(200).json({status:true,result});
+          
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
+
+module.exports = GetFacebookData;    
