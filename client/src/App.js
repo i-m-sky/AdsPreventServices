c490b54693c30157b54bb50e1a5fde3cfb34985d
Akash kumar <akashbhopewal@gmail.com>
2022-03-26 22:37:25 +0530

    apply react-toast funtion

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,11 @@ import Footer from './components/Footer';
 import AllRoutes from './components/AllRoutes/AllRoutes';
 import { useSelector} from 'react-redux';
 import Modal from 'react-modal';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Route } from 'react-router-dom';
 Modal.setAppElement('#root');
-
-
+toast.configure()
 const App = () => {
  
 const { user,loading} = useSelector((state) => state.authReducer);
